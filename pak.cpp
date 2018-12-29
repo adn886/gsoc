64177f6e61c398942b3703dcbea1dbf8ed505e4d
adn886 <adshe886@gmail.com>
2018-12-30 06:23:46 +1200

    firt commit

---
+++ pak.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,i,k,f=0;
+    cin>>n1>>n2;
+    for(i=n1;i<=n2;i++)
+    {
+        for(k=2;k<=n1/2;k++)
+        if(i%2!=0)
+        {
+       
+         cout<<i;
+         break;
+        }
+            
+        
+       
+}
+}
